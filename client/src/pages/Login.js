664378bf20113845492ff3eaa9b550066e4814b9
Kody Bishop <kodypbishop@gmail.com>
2020-04-04 14:45:38 -0700

    setup very basic server

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,21 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Nav from "../components/Nav";
+import API from "../utils/API";
 
 
 
 function Login() {
+    
+
+    function loadTest() {
+        API.test()
+          .then(res => 
+            console.log(res)
+          )
+          .catch(err => console.log(err));
+      };
+      loadTest();
+
     return (
         <div>
-            <Nav />
             <h1 className="text-center">Login Page</h1>
             <form>
                 <div className="form-group">
-                    <label for="exampleInputEmail1">Username</label>
+                    <label htmlFor="exampleInputEmail1">Username</label>
                     <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
                 </div>
                 <div className="form-group">
-                    <label for="exampleInputPassword1">Password</label>
+                    <label htmlFor="exampleInputPassword1">Password</label>
                     <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
                 </div>
                
