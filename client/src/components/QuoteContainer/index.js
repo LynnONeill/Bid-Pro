4a3b1e845ec7ac00710d3fb8db2d8e5def2ed8b6
Lynn O'Neill <loneill@afgaz.com>
2020-04-18 17:52:23 -0700

    quote container

--- client/src/components/QuoteContainer/index.js
+++ client/src/components/QuoteContainer/index.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import Row from "react-bootstrap/Row";
 import QuoteForm from "../QuoteForm";
 import QuoteHeaderText from "../QuoteHeaderText";
+import ClientCard from "../ClientCard";
+import ClientContext from "../../utils/API";
 
 
 
 function QuoteContainer({productDrop, designDrop, backingDrop, finishDrop, hardwareDrop, selectedProduct, selectedProductPX, handleProductSelect, handleFeatureSelect, backingPrice, finishPrice, designPrice, hardwarePrice, totalPrice, addProduct}) {
-
+    const { selectedClient } = useContext(ClientContext);
     return (
 
         <div className="jumbotron card-container">
+             <Row className="justify-content-md-center">
+                <ClientCard />
+             </Row>
             <Row className="justify-content-md-center">
                 <QuoteHeaderText 
                     selectedProduct={selectedProduct}
