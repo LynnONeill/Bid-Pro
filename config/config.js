7027a36c16f709855f3bc7d80f6272c6aa20444a
Lynn O'Neill <loneill@afgaz.com>
2020-04-23 21:10:19 -0700

    help

--- config/config.js
+++ config/config.js
@@ -2,17 +2,17 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    "username": "root",
-    "password": "rootroot",
-    "database": "bid_pro",
-    "host": "127.0.0.1",
+    "username": 'y9xsknin3ts5a24s',
+    "password": 'gqsfx0u7t88rc2vh',
+    "database": 'ci86rbkrjlxd5bzb',
+    "host": 'e764qqay0xlsc4cz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": "rootroot",
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": 'y9xsknin3ts5a24s',
+    "password": 'gqsfx0u7t88rc2vh',
+    "database": 'ci86rbkrjlxd5bzb',
+    "host": 'e764qqay0xlsc4cz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     "dialect": "mysql"
   },
   "production": {
@@ -21,6 +21,21 @@ module.exports = {
     "database": 'ci86rbkrjlxd5bzb',
     "host": 'e764qqay0xlsc4cz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     "dialect": "mysql"
-},
 }
+}
+
+// "development": {
+//   "username": "root",
+//   "password": "rootroot",
+//   "database": "bid_pro",
+//   "host": "127.0.0.1",
+//   "dialect": "mysql"
+// },
+// "test": {
+//   "username": "root",
+//   "password": "rootroot",
+//   "database": "database_test",
+//   "host": "127.0.0.1",
+//   "dialect": "mysql"
+// },
 
